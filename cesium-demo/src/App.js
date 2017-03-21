ea9a83a085da4b70428bd1df95a67778f9fc0ab1
Ben Ellentuck <ellentuckben@gmail.com>
2017-03-21 11:43:37 -0400

    Updating .gitignore and tweaking appearance

--- cesium-demo/src/App.js
+++ cesium-demo/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Viewer from 'cesium/Source/Widgets/Viewer/Viewer';
 
 class App extends Component {
+  componentDidMount() {
+    this.viewer = new Viewer(this.cesiumContainer);
+  }
+
   render() {
     return (
       <div>
-      <h2>Cesium Demo: to come</h2>
+      <div className="App-header">
+      <h2 className="App-intro">Cesium Demo</h2>
+      </div>
+      <div id='cesiumContainer' ref={ element => this.cesiumContainer = element }/>
       <ol>
         <li>
         http://blog.isquaredsoftware.com/2017/03/declarative-earth-part-1-cesium-webpack/
--- cesium-demo/src/index.js
+++ cesium-demo/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'cesium/Source/Widgets/widgets.css';
+import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
+
+//corresponds to the pre-built Cesium folder we copied earlier into $PROJECT/public:
+buildModuleUrl.setBaseUrl('./cesium/');
 
 ReactDOM.render(
   <App />,
