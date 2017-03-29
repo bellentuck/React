be5cde30acb33c8e6f76a242eb382cf949613e61
Ben Ellentuck <ellentuckben@gmail.com>
2017-03-29 15:47:58 -0400

    Rework layout styling to show content over the globe

--- cesium-demo/src/App.js
+++ cesium-demo/src/App.js
@@ -4,12 +4,30 @@ import CesiumGlobe from './cesium/CesiumGlobe';
 
 class App extends Component {
   render() {
+    const containerStyle = {
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      position: 'fixed',
+    };
+
     return (
       <div>
         <div className="App-header">
           <h2 className="App-intro">Cesium Demo</h2>
         </div>
-        <CesiumGlobe />
+
+        <div style={containerStyle}>
+          <CesiumGlobe />
+          <div style={{position: 'fixed', top: 0}}>
+            <div style={{color: 'white', fontSize: 40}}>
+              Text Over the Globe
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- cesium-demo/src/cesium/CesiumGlobe.js
+++ cesium-demo/src/cesium/CesiumGlobe.js
@@ -62,7 +62,7 @@ export default class CesiumGlobe extends Component {
           </span>
         );
       }
-      
+
       return contents;
     }
 
@@ -77,11 +77,6 @@ export default class CesiumGlobe extends Component {
         const containerStyle = {
             width: '100%',
             height: '100%',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'fixed',
             display : "flex",
             alignItems : "stretch",
         };
